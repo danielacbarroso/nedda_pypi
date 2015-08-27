859b81c071bf572cf15d7dc492c47c87fc7034e3
rvnovaes <roberto.novaes@silexsistemas.com.br>
2015-08-27 09:46:54 -0300

    TRY-283:adicionado novo arquivo CSV que relaciona com os ICDs

---
+++ web_service/tnm/templates/meu_javascript.js
@@ -0,0 +1,29 @@
+/**
+ * @param string url - URL que processara a requisicao do cliente
+ * @param string objHtmlReturn - ID do objeto HTML onde sera exibido o resultado
+ * @param int id - Valor enviado como base para obter um retorno
+ */
+function comboAjax(url,objHtmlReturn,id)
+{
+  // Cria uma variável dados em formato JSON, com 1 chave e 1 valor
+  dados = {'id':id};
+
+  // É inserido um elemento option dentro do elemento select
+  $("#"+objHtmlReturn).html('<option value="0">Carregando...</option>');
+  $.ajax({
+      type: "POST",
+      url: url,
+      dataType: "json",
+      data: dados,
+      success: function(retorno){
+          $("#"+objHtmlReturn).empty();
+          $.each(retorno, function(i, item){
+              $("#"+objHtmlReturn).append('<option value="'+item.pk+'">'+item.fields[fieldreturn]+'</option>');
+
+          });
+      },
+      error: function(erro) {
+        alert('Erro. Sem retorno da requisicao.');
+      }
+  });
+}
