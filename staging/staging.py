5ab037fbe51239d5a6a30246a9265c8ddc1909a1
danielacbarroso <sdanielabarroso@gmail.com>
2015-08-31 13:00:32 -0300

    TRY-283: Ajustes e limpeza do c?digo.

--- staging/staging.py
+++ staging/staging.py
@@ -4,13 +4,13 @@ import csv
 import re
 
 STAGES = list()
-TUMOR_t = list()
-NODES_n = list()
-METASTASES_m = list()
+# TUMOR_t = list()
+# NODES_n = list()
+# METASTASES_m = list()
 NEOPLASMS_c = list()
-DUKES_dukes = list()
-PSA_psa = list()
-GLEASON_gleason = list()
+# DUKES_dukes = list()
+# PSA_psa = list()
+# GLEASON_gleason = list()
 vetor = []
 
 with open(os.path.dirname(os.path.abspath(__file__)) + '/data/staging/icdname.csv', 'rt') as csvfile:
@@ -39,41 +39,12 @@ with open(os.path.dirname(os.path.abspath(__file__)) + '/data/staging/stages.csv
             'neoplasms': row[9]
             })
 
-        TUMOR_t.append({
-            'icd': row[0],
-            't': row[1]
-            })
-
-        NODES_n.append({
-            'icd': row[0],
-            'n': row[2]
-            })
-
-        METASTASES_m.append({
-            'icd': row[0],
-            'm': row[3]
-            })
-
-        DUKES_dukes.append({
-            'icd': row[0],
-            'dukes': row[4],
-        })
-
-        PSA_psa.append({
-            'icd': row[0],
-            'psa': row[6],
-        })
-
-        GLEASON_gleason.append({
-            'icd': row[0],
-            'psa': row[7],
-        })
 
 class GenericStager(object):
     
-    t_set = set()
-    n_set = set()
-    m_set = set()
+    _t_set = set()
+    _n_set = set()
+    _m_set = set()
     dukes_set = set()
     psa_set = set()
     gleason_set = set()
@@ -88,7 +59,7 @@ class GenericStager(object):
         self.stage = None
         self.icd = icd.split('.')[0].upper() # considers only the first part o ICD
         self.t = t
-        self. n = n
+        self.n = n
         self.m = m
         self.dukes = dukes
         self.psa = psa
@@ -99,15 +70,27 @@ class GenericStager(object):
         for item in STAGES:
             if self.icd == item['icd']:
                  if item['t'] is None:
-                     self.t_set.add(STAGES[item['icd']])
+                     self._t_set.add(STAGES[item['icd']])
                  else:
-                     self.t_set.add(item['t'])
+                     self._t_set.add(item['t'])
 
-                 self.n_set.add(item['n'])
-                 self.m_set.add(item['m'])
-                 self.dukes_set.add(item['dukes'])
-                 self.psa_set.add(item['psa'])
-                 self.gleason_set.add(item['gleason'])
+                 self._n_set.add(item['n'])
+                 self._m_set.add(item['m'])
+
+                 if item['dukes'] == '':
+                     self.dukes_set.add('-')
+                 else:
+                     self.dukes_set.add(item['dukes'])
+
+                 if item['psa'] == '':
+                     self.psa_set.add('-')
+                 else:
+                     self.psa_set.add(item['psa'])
+
+                 if item['gleason'] == '':
+                     self.gleason_set.add('-')
+                 else:
+                     self.gleason_set.add(item['gleason'])
                  self.carcinosarcoma_set.add(item['carcinosarcoma'])
                  self.neoplasms_set.add(item['neoplasms'])
                  self.stages_dict[item['t'] + item['n'] + item['m'] + item['dukes'] + item['psa'] + item['gleason']
@@ -116,6 +99,32 @@ class GenericStager(object):
         if self.t is not None:
             self.validate_tnm()
             self.staging()
+
+    def get_t_set(self):
+        return self._t_set
+
+    def get_t_list(self):
+        ordered_t_list = sorted(self.get_t_set())
+        tis = 'Tis'
+        #opcao usada para colocar o Tis como primeiro, caso exista
+        try:
+            del ordered_t_list[ordered_t_list.index(tis)]
+            ordered_t_list = [tis] + ordered_t_list
+            return ordered_t_list
+        except ValueError:
+            return ordered_t_list
+
+
+    def get_n_set(self):
+        return self._n_set
+    def get_n_list(self):
+        return sorted(self.get_n_set())
+
+    def get_m_set(self):
+        return self._m_set
+    def get_m_list(self):
+        return sorted(self.get_m_set())
+
 
     def staging(self):
         if self.t is None:
@@ -164,161 +173,68 @@ class GenericStager(object):
             self.stage = None
 
     def validate_tnm(self):
-        if self.t not in self.t_set:
+        if self.t not in self._t_set:
             self.valid = False
-            self.validation_message = 'Invalid T: ' + self.t + ' for ICD: ' + str(self.icd) + '. Valid Ts are ' + str(self.t_set)
-        if self.n not in self.n_set:
+            self.validation_message = 'Invalid T: ' + self.t + ' for ICD: ' + str(self.icd) + '. Valid Ts are ' + str(self._t_set)
+        if self.n not in self._n_set:
             self.valid = False
-            self.validation_message = self.validation_message + '\nInvalid N: ' + str(self.n) + ' for ICD: ' + str(self.icd) + '. Valid Ns are ' + str(self.n_set)
-        if self.m not in self.m_set:
+            self.validation_message = self.validation_message + '\nInvalid N: ' + str(self.n) + ' for ICD: ' + str(self.icd) + '. Valid Ns are ' + str(self._n_set)
+        if self.m not in self._m_set:
             self.valid = False
-            self.validation_message = self.validation_message + '\nInvalid M: ' + str(self.m) + ' for ICD: ' + str(self.icd) + '. Valid Ms are ' + str(self.m_set)
+            self.validation_message = self.validation_message + '\nInvalid M: ' + str(self.m) + ' for ICD: ' + str(self.icd) + '. Valid Ms are ' + str(self._m_set)
         if self.valid:
             self.validation_message = 'Valid TNM'
 
 def tnm_stage(icd, t=None, n=None, m=None, dukes=None, psa=None, gleason=None, carcinosarcoma=None, neoplasms=None):
     icd = icd.strip()
-    icd = icd.split(' - ')[0].upper()
+    #icd = icd.split(' - ')[0].upper()
     stager = GenericStager(icd, t, n, m, dukes, psa, gleason, carcinosarcoma, neoplasms)
     return stager.stage
 
-def tnm_neoplasms(neoplasms=None, Campo = None):
+def tnm_neoplasms(icd_neoplasm=None, Campo = None):
+
     retornar = []
     vetor = []
-    codigo = neoplasms
+    codigo = icd_neoplasm
 
     vetor = NEOPLASMS_c
 
     for i in range(0, len(vetor)):
         valor = vetor[i]
 
-        if neoplasms is not None:
+        if icd_neoplasm is not None:
             if valor['ICD'] == codigo.split(' - ')[0].upper():
                 if Campo is not None:
                     if Campo.upper() == 'ICD':
-                        if tnm_t(valor['ICD']):
+                        if gs.get_t_list(valor['ICD']):
                             retornar.append(valor['ICD'])
                     elif Campo.upper() == 'NEOPLASMS':
-                        if tnm_t(valor['ICD']):
+                        if gs.get_t_list(valor['ICD']):
                             retornar.append(valor['neoplasms'])
                     else:
-                        if tnm_t(valor['ICD']):
+                        if gs.get_t_list(valor['ICD']):
                             retornar.append(valor['ICD'] + ' - ' + valor['neoplasms'])
                 else:
-                    if tnm_t(valor['ICD']):
+                    if gs.get_t_list(valor['ICD']):
                         retornar.append(valor['ICD'] + ' - ' + valor['neoplasms'])
 
             retornar = list(set(retornar))
 
         else:
+
             if Campo is not None:
+                gs = GenericStager()
                 if Campo.upper() == 'ICD':
-                    if tnm_t(valor['ICD']):
+                    if gs.get_t_list(valor['ICD']):
                         retornar.append(valor['ICD'])
                 elif Campo.upper() == 'NEOPLASMS':
-                    if tnm_t(valor['ICD']):
+                    if gs.get_t_list(valor['ICD']):
                         retornar.append(valor['neoplasms'])
                 else:
-                    if tnm_t(valor['ICD']):
+                    if gs.get_t_list(valor['ICD']):
                         retornar.append(valor['ICD'] + ' - ' + valor['neoplasms'])
             else:
-                if tnm_t(valor['ICD']):
+                if gs.get_t_list(valor['ICD']):
                     retornar.append(valor['ICD'] + ' - ' + valor['neoplasms'])
 
-    return sorted(retornar)
-
-def tnm_t(icd):
-    retornar = []
-    vetor = []
-    codigo = icd.split('.')[0].upper()
-    for i in range(1, len(TUMOR_t)):
-        num = TUMOR_t[i]
-        vetor.append(num)
-
-    for i in range(1, len(vetor)):
-        valor = vetor[i]
-        if valor['icd'] == codigo.split(' - ')[0].upper():
-            retornar.append(valor['t'])
-
-    retornar = list(set(retornar))
-    return sorted(retornar)
-
-def tnm_n(icd):
-    retornar = []
-    vetor = []
-    codigo = icd.split('.')[0].upper()
-    for i in range(1, len(NODES_n)):
-        num = NODES_n[i]
-        vetor.append(num)
-
-    for i in range(1, len(vetor)):
-        valor = vetor[i]
-        if valor['icd'] == codigo.split(' - ')[0].upper():
-            retornar.append(valor['n'])
-
-    retornar = list(set(retornar))
-    return sorted(retornar)
-
-def tnm_m(icd):
-    retornar = []
-    vetor = []
-    codigo = icd.split('.')[0].upper()
-    for i in range(1, len(METASTASES_m)):
-        num = METASTASES_m[i]
-        vetor.append(num)
-
-    for i in range(1, len(vetor)):
-        valor = vetor[i]
-        if valor['icd'] == codigo.split(' - ')[0].upper():
-            retornar.append(valor['m'])
-
-    retornar = list(set(retornar))
-    return sorted(retornar)
-
-def tnm_dukes(icd):
-    retornar = []
-    vetor = []
-    codigo = icd.split('.')[0].upper()
-    for i in range(1, len(DUKES_dukes)):
-        num = DUKES_dukes[i]
-        vetor.append(num)
-
-    for i in range(1, len(vetor)):
-        valor = vetor[i]
-        if valor['icd'] == codigo.split(' - ')[0].upper():
-            retornar.append(valor['dukes'])
-
-    retornar = list(set(retornar))
-    return sorted(retornar)
-
-def tnm_psa(icd):
-    retornar = []
-    vetor = []
-    codigo = icd.split('.')[0].upper()
-    for i in range(1, len(PSA_psa)):
-        num = PSA_psa[i]
-        vetor.append(num)
-
-    for i in range(1, len(vetor)):
-        valor = vetor[i]
-        if valor['icd'] == codigo:
-            retornar.append(valor['psa'])
-
-    retornar = list(set(retornar))
-    return sorted(retornar)
-
-def tnm_gleason(icd):
-    retornar = []
-    vetor = []
-    codigo = icd.split('.')[0].upper()
-    for i in range(1, len(GLEASON_gleason)):
-        num = GLEASON_gleason[i]
-        vetor.append(num)
-
-    for i in range(1, len(vetor)):
-        valor = vetor[i]
-        if valor['icd'] == codigo:
-            retornar.append(valor['gleason'])
-
-    retornar = list(set(retornar))
     return sorted(retornar)
