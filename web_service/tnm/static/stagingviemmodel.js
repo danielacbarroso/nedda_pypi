7d667b0506dabde837d9c3cb71d2db27bf875f25
danielacbarroso <sdanielabarroso@gmail.com>
2015-10-05 10:23:16 -0400

    TRY-284:Server configurations

---
+++ web_service/tnm/static/stagingviemmodel.js
@@ -0,0 +1,144 @@
+$(function() {
+    StagingViewModel = {
+
+        available_icds_neoplasms: ko.observableArray([]),
+        available_icds: ko.observableArray([]),
+        available_ts:  ko.observableArray([]),
+        available_ns:  ko.observableArray([]),
+        available_ms:  ko.observableArray([]),
+        available_dukes: ko.observableArray([]),
+        available_psa: ko.observableArray([]),
+        available_gleason: ko.observableArray([]),
+
+        current_icds_neoplasm: ko.observable(''),
+        current_icd: ko.observable(''),
+        current_t:  ko.observable(''),
+        current_n:  ko.observable(''),
+        current_m:  ko.observable(''),
+        current_dukes: ko.observable(''),
+        current_psa: ko.observable(''),
+        current_gleason: ko.observable(''),
+
+        calculated_stage: ko.observable(''),
+
+        loadInitialState: function () {
+            $('#l_dukes').hide()
+            $('#dukes_selector').hide();
+            $('#l_psa').hide()
+            $('#psa_selector').hide();
+            $('#l_gleason').hide()
+            $('#gleason_selector').hide();
+            $('#b_stage').hide();
+            $.getJSON('get_icds', function (json) {
+                StagingViewModel.available_icds(json.icd_list);
+                //StagingViewModel.available_icds_neoplasms(json.icd_list_neoplasm);
+            });
+        },
+
+        icdChanged: function () {
+            console.log(StagingViewModel.current_icd());
+            $('#b_stage').hide();
+            StagingViewModel.available_ts([]);
+            StagingViewModel.available_ns([]);
+            StagingViewModel.available_ms([]);
+            StagingViewModel.available_dukes([]);
+            StagingViewModel.available_psa([]);
+            StagingViewModel.available_gleason([]);
+            StagingViewModel.calculated_stage('');
+
+
+            if(StagingViewModel.current_icd() === 'C18 - Colorectal - Colon'){
+                $('#l_dukes').show()
+                $('#dukes_selector').show();
+            }
+            else{
+                $('#l_dukes').hide()
+                $('#dukes_selector').hide();
+            }
+
+            if(StagingViewModel.current_icd() === 'C61 - Prostate'){
+
+                $('#l_psa').show();
+                $('#psa_selector').show();
+                $('#l_gleason').show();
+                $('#gleason_selector').show();
+            }
+            else{
+                $('#l_psa').hide();
+                $('#psa_selector').hide();
+                $('#l_gleason').hide();
+                $('#gleason_selector').hide();
+            }
+
+            $.getJSON('get_tnms/' + StagingViewModel.current_icd(), function (json) {
+
+                StagingViewModel.available_ts(json.ts_list);
+                StagingViewModel.available_ns(json.ns_list);
+                StagingViewModel.available_ms(json.ms_list);
+                StagingViewModel.available_dukes(json.dukes_list);
+                StagingViewModel.available_psa(json.psa_list);
+                StagingViewModel.available_gleason(json.gleason_list);
+
+            });
+            },
+
+        tnmChanged:function() {
+            if (StagingViewModel.current_icd() === 'C18 - Colorectal - Colon') {
+                if (StagingViewModel.current_t() !== undefined &&
+                StagingViewModel.current_n() !== undefined &&
+                StagingViewModel.current_m() !== undefined &&
+                StagingViewModel.current_dukes() !== undefined) {
+
+                $.getJSON('get_stage/' + StagingViewModel.current_icd() + '/' + StagingViewModel.current_t() + '/' +
+                    StagingViewModel.current_n() + '/' + StagingViewModel.current_m() +
+                    '/' + StagingViewModel.current_dukes() + '/'+
+                    StagingViewModel.current_psa() + '/' + StagingViewModel.current_gleason(), function (json) {
+                    $('#b_stage').show();
+                    StagingViewModel.calculated_stage(json.stage);
+                    });
+                }//fim if interno
+
+
+            }//fim if C18
+            else if (StagingViewModel.current_icd() === 'C61 - Prostate') {
+                if (StagingViewModel.current_t() !== undefined &&
+                StagingViewModel.current_n() !== undefined &&
+                StagingViewModel.current_m() !== undefined &&
+                StagingViewModel.current_psa() !== undefined &&
+                StagingViewModel.current_gleason() !== undefined) {
+
+                $.getJSON('get_stage/' + StagingViewModel.current_icd() + '/' + StagingViewModel.current_t() + '/' +
+                    StagingViewModel.current_n() + '/' + StagingViewModel.current_m() +
+                    '/' + StagingViewModel.current_dukes() + '/'+
+                    StagingViewModel.current_psa() + '/' + StagingViewModel.current_gleason(), function (json) {
+                    $('#b_stage').show();
+                    StagingViewModel.calculated_stage(json.stage);
+                    });
+                }//fim if interno
+
+            }//fim if C61
+            else {
+
+            // resto CID
+            if (StagingViewModel.current_icd() !== undefined &&
+                StagingViewModel.current_t() !== undefined &&
+                StagingViewModel.current_n() !== undefined &&
+                StagingViewModel.current_m() !== undefined) {
+
+                $.getJSON('get_stage/' + StagingViewModel.current_icd() + '/' + StagingViewModel.current_t() + '/' +
+                    StagingViewModel.current_n() + '/' + StagingViewModel.current_m() +
+                    '/' + StagingViewModel.current_dukes() + '/' +
+                    StagingViewModel.current_psa() + '/' + StagingViewModel.current_gleason(), function (json) {
+                    $('#b_stage').show();
+                    StagingViewModel.calculated_stage(json.stage);
+                });
+            }//fim if interno
+            }// fim if resto CID
+
+
+        }
+    };
+
+    ko.applyBindings(StagingViewModel);
+    StagingViewModel.loadInitialState();
+});
---
+++ web_service/tnm/views.py
@@ -0,0 +1,79 @@
+import json
+from django.shortcuts import render
+from django.http import HttpResponse
+from staging.staging import GenericStager
+from staging.staging import STAGES
+from staging.staging import tnm_stage
+
+icd_list_set = set()
+
+
+def calcula(request):
+    return render(request, "calcula.html")
+
+
+def get_icds(request):
+
+    icd_list = []
+
+    if len(icd_list_set) == 0:
+        for item in STAGES:
+            icd_list.append(item['icd'] + ' - ' + item['neoplasms'])
+
+        icd_list = set(icd_list)
+
+    request_data = {
+        'request_status': 'success',
+        'icd_list': sorted(icd_list),
+    }
+
+    return HttpResponse(json.dumps(request_data), content_type='application/json')
+
+
+def get_tnms(request, icd):
+
+    icd = icd.split('-')[0].strip()
+
+    gs = GenericStager(icd)
+
+    request_data = {
+        'request_status': 'success',
+        'ts_list': gs.get_t_list(),
+        'ns_list': gs.get_n_list(),
+        'ms_list': gs.get_m_list(),
+        'dukes_list': gs.get_dukes_list(),
+        'psa_list': gs.get_psa_list(),
+        'gleason_list': gs.get_gleason_list(),
+    }
+
+    return HttpResponse(json.dumps(request_data), content_type='application/json')
+
+
+def get_stage(request, icd, t, n, m, dukes=None, psa=None, gleason=None):
+
+    if dukes == u'undefined':
+        dukes = None
+    if psa == u'undefined':
+        psa = None
+    if gleason == u'undefined':
+        gleason = None
+
+    if icd == 'C54 - Corpus Uteri Carcinomas':
+        carcinosarcoma = 'C'
+    elif icd == 'C54 - Corpus Uteri Sarcomas':
+        carcinosarcoma = 'S'
+    else:
+        carcinosarcoma = None
+
+    stage = tnm_stage(icd, t, n, m, dukes=dukes, psa=psa, gleason=gleason, carcinosarcoma=carcinosarcoma)
+
+    if stage == None:
+        stage = 'undefined'
+    stage = 'Staging ' + stage
+
+    request_data = {
+        'request_status': 'success',
+        'stage': stage
+    }
+
+    return HttpResponse(json.dumps(request_data), content_type='application/json')
